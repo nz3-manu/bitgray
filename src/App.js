56c3d6808e9f06d99789738a41f37a31323ef301
Manuel Alejandro Ramirez Cruz <nz3.manu@gmail.com>
2017-01-15 21:35:02 -0500

    aside new style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-    render() {
+var Album =React.createClass({
+    render: function() {
         return (
+            <div className="album col-xs-12 col-sm-6 col-md-4">
+                <div className="album__effect">
+                    <img src="http://lorempixel.com/700/700/" className="img-responsive img-thumbnail" alt=""/>
+                </div>
+                <div className="album__title"> <h5 className="text-center">{this.props.titleAlbum}</h5></div>
+            </div>
+        )
+    }
+});
 
+var AsideInformation=React.createClass({
+    render: function() {
+        return (
+            <div className="main__asideInformation">
+                <h3>Nombre:</h3>
+                <p className="personal__information">{this.props.name}</p>
+                <h3>Usuario:</h3>
+                <p className="personal__information">{this.props.user}</p>
+                <h3>Correo:</h3>
+                <p className="personal__information">{this.props.email}</p>
+                <h3>Telefono:</h3>
+                <p className="personal__information">{this.props.phone}</p>
+                <h3>Compañia:</h3>
+                <p className="personal__information">{this.props.company}</p>
+            </div>
+        )
+    }
+});
+
+var App = React.createClass({
+    getInitialState: function() {
+        return {users: [], album: []}
+    },
+    componentDidMount: function() {
+        fetch("http://jsonplaceholder.typicode.com/users").then(userJson => userJson.json(), e => {
+            console.log("Obtención fallida", e);
+        }).then(userJson => {
+            let longUser=userJson.length
+            let randomMethod=Math.floor(Math.random() * (longUser));
+            this.setState({users: userJson[randomMethod]})
+        });
+        fetch("http://jsonplaceholder.typicode.com/albums").then(albumJson => albumJson.json(), e => {
+            console.log("Obtención fallida", e);
+        }).then(albumJson => {
+            this.setState({album: albumJson})
+        });
+
+    },
+    randomInformation: function() {
+        console.log("hola mundo");
+    },
+    render: function() {
+        return (
             <div>
                 <header>
                     <div className="container">
@@ -17,55 +68,32 @@ class App extends Component {
 
                 <div className="container">
                     <section className="main row overflow">
-                        <aside className="col-xs-12 col-sm-4 col-md-3">
-                            <h3>  Nombre: </h3>
-                            <h3> Usuario: </h3>
-                            <h3> Correo: </h3>
-                            <h3> Telefono: </h3>
-                            <h3> Compañia: </h3>
+                        <aside className="col-xs-12 col-sm-5">
+                          <div className="main__information">
+                            <h3>INFORMACION PERSONAL</h3>
+                          </div>
+                            < AsideInformation name={(this.state.users.name)
+                                ? this.state.users.name
+                                : ""} user={(this.state.users.username)
+                                ? this.state.users.username
+                                : ""} email={(this.state.users.email)
+                                ? this.state.users.email
+                                : ""} phone={(this.state.users.phone)
+                                ? this.state.users.phone
+                                : ""} company={(this.state.users.company)
+                                ? this.state.users.company.name
+                                : ""}/>
                         </aside>
-                        <article className="col-xs-12 col-sm-8 col-md-9">
-                            <div className="row overflow">
-                                <div className="album col-xs-12 col-sm-6 col-md-4">
-                                    <h3 className="text-center">
-                                        Title de muestra
-                                    </h3>
-                                    <div className="album__effect">
-                                        <img src="http://lorempixel.com/700/700/" className="img-responsive img-thumbnail" alt=""/>
-                                    </div>
-                                </div>
-                                <div className="album col-xs-12 col-sm-6 col-md-4">
-                                    <h3 className="text-center">
-                                        Title de muestra
-                                    </h3>
-                                    <div className="album__effect">
-                                      <img src="http://lorempixel.com/700/700/" className="img-responsive img-thumbnail" alt=""/>
-                                    </div>
-                                </div>
-                                <div className="album col-xs-12 col-sm-6 col-md-4">
-                                    <h3 className="text-center">
-                                        Title de muestra
-                                    </h3>
-                                    <div className="album__effect">
-                                       <img src="http://lorempixel.com/700/700/" className="img-responsive img-thumbnail" alt=""/>
-                                    </div>
-                                </div>
-                                <div className="album col-xs-12 col-sm-6 col-md-4">
-                                    <h3 className="text-center">
-                                        Title de muestra
-                                    </h3>
-                                    <div className="album__effect">
-                                       <img src="http://lorempixel.com/700/700/" className="img-responsive img-thumbnail" alt=""/>
-                                    </div>
-                                </div>
-                                <div className="album col-xs-12 col-sm-6 col-md-4">
-                                    <h3 className="text-center">
-                                        Title de muestra
-                                    </h3>
-                                    <div className="album__effect">
-                                       <img src="http://lorempixel.com/700/700/" className="img-responsive img-thumbnail" alt=""/>
-                                    </div>
-                                </div>
+
+                        <article className="col-xs-12 col-sm-7">
+                            <div className="row overflow ">
+                                {this.state.album.map((album, i)=>{
+                                    if (this.state.users.id===album.userId) {
+                                      return < Album titleAlbum={album.title} key={i}/>
+                                }
+                            })
+}
+
                             </div>
                         </article>
                     </section>
@@ -73,10 +101,13 @@ class App extends Component {
 
                 <footer>
                     <div className="container displayFlex">
-                        <h4><strong>Bitgray app prueba tecnica</strong></h4>
-                          <div className="">
+                        <h4>
+                            <strong>Prueba tecnica Bitgray app
+                            </strong>
+                        </h4>
+                        <div className="">
                             <img src="logo-whitebig.png" width="55px" height="55px" alt="Logo__footer"/>
-                          </div>
+                        </div>
                     </div>
                 </footer>
 
@@ -84,6 +115,6 @@ class App extends Component {
 
         );
     }
-}
+})
 
 export default App;
