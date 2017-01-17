e93321374a09bc156b5be8cc539eb4ffa538eef2
Manuel Alejandro Ramirez Cruz <nz3.manu@gmail.com>
2017-01-17 10:54:47 -0500

    added style component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
-
-
+import {RouteTransition} from 'react-router-transition';
 
 var App = React.createClass({
     render: function() {
         return (
             <div>
-              <Header/>
-                {this.props.children}
-              <Footer/>
+                <Header/>
+                <RouteTransition pathname={this.props.location.pathname}
+                  atEnter={{
+                    opacity: 0
+                }} atLeave={{
+                    opacity: 0
+                }} atActive={{
+                    opacity: 1
+                }}>
+                    {this.props.children}
+                </RouteTransition>
+                <Footer/>
             </div>
 
         );
